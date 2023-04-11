13a0d5f7c37e928f510ab8d6f6aca01dde275d60
Panagiotis Linardos Panagiotis.Linardos@Pennmedicine.upenn.edu <linardop@cubic-login5.bicic.local>
2023-04-11 11:23:21 -0400

    runner.sh

--- runner.sh
+++ runner.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
-#$ -l h_vmem=100G ## amout RAM being requested
+#$ -l sgx
+## $ -l h_vmem=100G ## amout RAM being requested
 ##$ -l gpu # request the more common P100 nodes
 ##$ -l A40 # to request the less common A40 nodes
 ## more details in https://sbia-wiki.uphs.upenn.edu/wiki/index.php/GPU_Computing
 #$ -pe threaded 10 ## change number of CPU threads you want to request here
+#$ -o test.out
+#$ -e test.err
 #$ -cwd
-#$ -m b 
-#$ -m e 
-# this file is used to run gpu jobs on the cluster in a proper manner so 
+#$ -m b
+#$ -m e
+# this file is used to run gpu jobs on the cluster in a proper manner so
 # that the CUDA_VISIBLE_DEVICES environment variable is properly initialized
 # ref: https://sbia-wiki.uphs.upenn.edu/wiki/index.php/GPU_Computing#Directing_Jobs_to_a_Specific_GPU_with_the_get_CUDA_VISIBLE_DEVICES_Utility
 ### $1: absolute path to python interpreter in virtual environment
@@ -17,13 +20,13 @@
 ### $5: output_dir (relative to cwd)
 ### $6: folder to copy to scratch space
 
-$1 ../tackle_scratch_space.py -g $2 -d $3 -c $4 -o $5 -f $6
+# $1 ../tackle_scratch_space.py -g $2 -d $3 -c $4 -o $5 -f $6
 
 # ## run actual trainer
-# $1 $2 \
-# --inputdata $3 \
-# --config $4 \
-# --modeldir $5 \
-# --train True \
-# --device cuda \
-# --reset True # this removes previously saved checkpoints and data
+$1 $2 \
+--inputdata $3 \
+--config $4 \
+--modeldir $5 \
+--train True \
+--device cpu \
+--reset True # this removes previously saved checkpoints and data
