687f58474a927a05a4c87f081d8f4c7dfa20d989
sarthakpati <sarthak.pati@hotmail.com>
2023-09-01 17:45:56 -0400

    removing things that weren't needed

--- runner.sh
+++ runner.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
 
-#SBATCH -J job_name
+###SBATCH -J job_name
 #SBATCH -p gpu
-#SBATCH -A slurm-account-name
+###SBATCH -A slurm-account-name
 #SBATCH -o filename_%j.txt
 #SBATCH -e filename_%j.err
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=username@iu.edu
+###SBATCH --mail-user=username@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=02:00:00
+###SBATCH --time=02:00:00
 
 #Load any modules that your program needs
 module load python/gpu/x.x.x ## check this
-
-#Run your program
-srun ./my_program my_program_arguments
-
-#! /bin/bash
-#$ -l h_vmem=100G ## amout RAM being requested
-##$ -l gpu # request the more common P100 nodes
-##$ -l A40 # to request the less common A40 nodes
-## more details in https://sbia-wiki.uphs.upenn.edu/wiki/index.php/GPU_Computing
-#$ -pe threaded 10 ## change number of CPU threads you want to request here
-#$ -cwd
-#$ -m b 
-#$ -m e 
-# this file is used to run gpu jobs on the cluster in a proper manner so 
-# that the CUDA_VISIBLE_DEVICES environment variable is properly initialized
-# ref: https://sbia-wiki.uphs.upenn.edu/wiki/index.php/GPU_Computing#Directing_Jobs_to_a_Specific_GPU_with_the_get_CUDA_VISIBLE_DEVICES_Utility
-### $1: absolute path to python interpreter in virtual environment
-### $2: absolute path to gandlf_run that needs to be invoked
-### $3: absolute path to the data.csv file
-### $4: yaml configuration
-### $5: output_dir (relative to cwd)
-### $6: folder to copy to scratch space
 
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
